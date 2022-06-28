595abb2f988c6fb807a06801c46cd39bdc4ddae8
Shiv Sundram <shiv1@sapling.stanford.edu>
2022-06-27 17:32:02 -0700

    sweeping code

--- examples/mandelbrot.py
+++ examples/mandelbrot.py
@@ -4,9 +4,11 @@
 # More information at https://github.com/rougier/numpy-book
 # -----------------------------------------------------------------------------
 
-#import cunumeric as np
-import numpy as np
-
+import cunumeric as np
+import numpy as npy
+import argparse
+import datetime
+import math 
 
 def linspace(start, stop, num, dtype):
     X = np.empty((num, ), dtype=dtype)
@@ -16,47 +18,108 @@ def linspace(start, stop, num, dtype):
     return X
 
 
-def mandelbrot(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=4.0):
+def mandelbrot(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=4.0, timing=True):
     # Adapted from https://www.ibm.com/developerworks/community/blogs/jfp/...
     #              .../entry/How_To_Compute_Mandelbrodt_Set_Quickly?lang=en
+
     X = linspace(xmin, xmax, xn, dtype=np.float64)
     Y = linspace(ymin, ymax, yn, dtype=np.float64)
     Y1 = Y[:, None]
     N = np.zeros((xn, yn), dtype=np.int64)
     Zre = np.zeros((xn, xn), dtype=np.float64)
     Zim = np.zeros((yn, yn), dtype=np.float64)
+    tmp = np.zeros((yn, yn), dtype=np.float64)
+    #ZimTemp = np.zeros((yn, yn), dtype=np.float64)
+    #I=np.zeros(N.shape, dtype=np.int64)
+    start = datetime.datetime.now()
     for n in range(maxiter):
         I = np.less(Zre*Zre + Zim*Zim, horizon)
-        N = np.int64(I)*n+np.int64(~I)*N
+        N = (I)*n+(~I)*N
 
         tmp = (Zre)*Zre - (Zim)*Zim + X
-        ZimTemp = 2 * Zre  * Zim + Y1
+        ZimTemp = 2.0 * Zre  * Zim + Y1
 
-        Zre[:] = tmp*np.int64(I) + Zre*np.int64(~I)
-        Zim[:] = ZimTemp*np.int64(I) + Zim*np.int64(~I)
+        Zre = tmp*(I) + Zre*(~I)
+        Zim = ZimTemp*(I) + Zim*(~I)
         
+    assert np.sum(N)>0
+    stop = datetime.datetime.now()
+    delta = stop - start
+    total = delta.total_seconds() * 1000.0
+    if timing:
+        print("Elapsed Time: " + str(total) + " ms")
     NmaxIter = (N==maxiter -1)
     N = np.int64(~NmaxIter)*N
-    return Zre, Zim, N
+    return Zre, Zim, N, total
 
-g=500
+g=2000
 xmin = -1.75
 xmax = .25
 xn = g
 ymin = -1.0
 ymax = 1.0
 yn=g
-maxiter=100
+maxiter=500
 
-for i in range(1):
-    Zre, Zim, N = mandelbrot(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=4.0)
-    #Z1, N1 = mandelbrotN(xmin, xmax, ymin, ymax, xn, yn, maxiter, horizon=2.0)
-from PIL import Image
-      
-# creating a image object
-image  = Image.new(mode="1", size=(xn, yn)) 
-N=(N%2==0)  
-N=np.array(N, dtype=bool)
-width, height = image.size
-data = Image.fromarray(N)
-data.save("data.png")
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-n",
+        "--ndim",
+        dest="ndim",
+        type=int,
+        default=1,
+        help="save iteration data",
+    )
+
+    parser.add_argument(
+        "-b",
+        "--benchmark",
+        dest="nbenchmark",
+        type=int,
+        default=3,
+        help="save iteration data",
+    )
+
+
+
+    parser.add_argument(
+        "-s",
+        "--save_data",
+        dest="save_data",
+        action="store_true",
+        default=False,
+        help="save iteration data",
+    )
+ 
+    parser.add_argument(
+        "-g",
+        "--genimage",
+        dest="make_image",
+        action="store_true",
+        default=False,
+        help="generate image",
+    )
+ 
+    args = parser.parse_args()
+    totals = []
+    for i in range(args.nbenchmark):
+        Zre, Zim, N, total = mandelbrot(xmin, xmax, ymin, ymax, args.ndim, args.ndim, maxiter, 4.0, True)
+        totals.append(total)
+    print("parsetotal", float(sum(totals))/len(totals)) 
+
+    if args.save_data:
+       with open("data.npy", 'wb') as f:
+           N=npy.array(N, dtype=np.int64)
+           np.save(f,N)
+
+    if args.make_image:
+        from PIL import Image
+        # creating a image object
+        image  = Image.new(mode="1", size=(args.ndim, args.ndim)) 
+        N=(N%2==0)  
+        print(N.type)
+        width, height = image.size
+        data = Image.fromarray(N)
+        data.save("data.png")
--- sweep.sh
+++ sweep.sh
@@ -1,9 +1,12 @@
 GPUSPERNODE=4
 for i in "1 188" "2 238" "4 300 8 378 16 400" 
 do
+    break
     set -- $i # convert the "tuple" into the param args $1 $2...
-    NODES=$(($1 / $GPUSPERNODE))
-    NODES=$(( $NODES > 1 ? $NODES : 1 ))
+    ZNODES=$(($1 / $GPUSPERNODE))
+    NODES=$(( $ZNODES > 1 ? $ZNODES : 1 ))
+    GPUSPERNODE=$(( $ZNODES > 1 ? $ZNODES : $1 ))
+
     WSIZE=50
     if [[ "$SLURM_JOB_NUM_NODES" -lt "$NODES" ]]; then
         break
@@ -13,4 +16,24 @@ do
     LEGATE_WINDOW_SIZE=$WSIZE ../legate.core/install/bin/legate examples/stencil_27.py -n $2 -i 500 -t -b 3 --gpus $GPUSPERNODE -ll:fsize 12000 --launcher mpirun --nodes $NODES | grep "parsetotal"
     echo running stencil with ngpus $1, $NODES nodes, size $2 , window size 1
     LEGATE_WINDOW_SIZE=1 ../legate.core/install/bin/legate examples/stencil_27.py -n $2 -i 500 -t -b 3 --gpus $GPUSPERNODE -ll:fsize 12000 --launcher mpirun --nodes $NODES | grep "parsetotal"
+done
+
+
+echo "do mandelbrot"
+for i in "1 1000" "4 2000" "8 2828"
+do
+    GPUSPERNODE=4
+    set -- $i # convert the "tuple" into the param args $1 $2...
+    ZNODES=$(($1 / $GPUSPERNODE))
+    NODES=$(( $ZNODES > 1 ? $ZNODES : 1 ))
+    GPUSPERNODE=$(( $ZNODES > 1 ? $ZNODES : $1 ))
+
+    WSIZE=50
+    if [[ "$SLURM_JOB_NUM_NODES" -lt "$NODES" ]]; then
+        break
+    fi
+    echo running mandel with ngpus $1, $NODES nodes, size $2 , window size $WSIZE
+    LEGATE_WINDOW_SIZE=50 ../legate.core/install/bin/legate examples/mandelbrot.py --gpus $GPUSPERNODE --nodes $NODES --launcher mpirun -n $2 | grep "parsetotal"
+    echo running mandel with ngpus $1, $NODES nodes, size $2 , window size 1
+    LEGATE_WINDOW_SIZE=1 ../legate.core/install/bin/legate examples/mandelbrot.py --gpus $GPUSPERNODE --nodes $NODES --launcher mpirun -n $2 | grep "parsetotal"
 done
